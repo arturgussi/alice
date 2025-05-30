5ce9922146f5db2d25e93438117300ccffd90d9b
Artur Gussi <arturgussi@gmail.com>
2025-05-30 13:40:27 -0300

    style: Estilização do Drawer Menu

--- src/navigation/routes/MainAppDrawer.tsx
+++ src/navigation/routes/MainAppDrawer.tsx
@@ -4,11 +4,20 @@ import {
   DrawerContentScrollView,
   DrawerItem,
 } from '@react-navigation/drawer';
+import {
+  getFocusedRouteNameFromRoute,
+  NavigationState,
+  PartialState,
+} from '@react-navigation/native';
+import React from 'react';
 import { Platform, StyleSheet, View } from 'react-native';
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 
 import { ThemedColors } from '@/constants/Theme.style';
-import { MainAppDrawerParamList } from '@/types/navigation/NavigationTypes';
+import {
+  MainAppDrawerParamList,
+  AppBottomTabParamList,
+} from '@/types/navigation/NavigationTypes';
 import { signOff } from '@services/auth/Auth';
 
 import { BottomTabNavigator } from './BottomTabNavigator';
@@ -20,6 +29,39 @@ function CustomDrawerContent(props: DrawerContentComponentProps) {
     await signOff();
   };
 
+  const drawerState = props.state;
+  const activeDrawerRoute = drawerState.routes[drawerState.index];
+
+  let activeTabName: keyof AppBottomTabParamList | undefined;
+  let activeScreenInTab: string | undefined;
+
+  if (
+    activeDrawerRoute.name === 'AppTabsContainer' &&
+    activeDrawerRoute.state
+  ) {
+    const tabNavigatorState = activeDrawerRoute.state as
+      | NavigationState<AppBottomTabParamList>
+      | PartialState<NavigationState<AppBottomTabParamList>>;
+
+    if (
+      typeof tabNavigatorState.index === 'number' &&
+      tabNavigatorState.routes[tabNavigatorState.index]
+    ) {
+      activeTabName = tabNavigatorState.routes[tabNavigatorState.index]
+        .name as keyof AppBottomTabParamList;
+
+      const activeTabRoute = tabNavigatorState.routes[tabNavigatorState.index];
+      activeScreenInTab = getFocusedRouteNameFromRoute(activeTabRoute);
+
+      if (activeTabName === 'HomeTab' && !activeScreenInTab)
+        activeScreenInTab = 'HomeMain';
+      if (activeTabName === 'MeasurementsTab' && !activeScreenInTab)
+        activeScreenInTab = 'MeasurementsList';
+      if (activeTabName === 'ProfileTab' && !activeScreenInTab)
+        activeScreenInTab = 'UserProfileView';
+    }
+  }
+
   return (
     <View style={drawerStyles.container}>
       <DrawerContentScrollView
@@ -28,76 +70,141 @@ function CustomDrawerContent(props: DrawerContentComponentProps) {
       >
         <DrawerItem
           label="Início"
-          labelStyle={{ color: ThemedColors.text }}
-          icon={({ size, color }) => (
-            <FontAwesomeIcon name="home" size={size} color={color} />
+          labelStyle={{
+            color:
+              activeTabName === 'HomeTab' && activeScreenInTab === 'HomeMain'
+                ? 'white'
+                : ThemedColors.text,
+          }}
+          focused={
+            activeTabName === 'HomeTab' && activeScreenInTab === 'HomeMain'
+          }
+          icon={({ focused, size }) => (
+            <FontAwesomeIcon
+              name="home"
+              size={size}
+              color={focused ? ThemedColors.lightPurple : ThemedColors.text}
+            />
           )}
+          activeBackgroundColor={ThemedColors.darkPurple}
           onPress={() =>
             props.navigation.navigate('AppTabsContainer', {
               screen: 'HomeTab',
-              params: {
-                screen: 'HomeMain',
-              },
+              params: { screen: 'HomeMain' },
             })
           }
         />
         <DrawerItem
           label="Medições"
-          labelStyle={{ color: ThemedColors.text }}
-          icon={({ size, color }) => (
-            <FontAwesomeIcon name="line-chart" size={size} color={color} />
+          labelStyle={{
+            color:
+              activeTabName === 'MeasurementsTab' &&
+              (activeScreenInTab === 'MeasurementsList' ||
+                activeScreenInTab === 'MeasurementDetail')
+                ? 'white'
+                : ThemedColors.text,
+          }}
+          focused={
+            activeTabName === 'MeasurementsTab' &&
+            (activeScreenInTab === 'MeasurementsList' ||
+              activeScreenInTab === 'MeasurementDetail')
+          }
+          icon={({ focused, size }) => (
+            <FontAwesomeIcon
+              name="line-chart"
+              size={size}
+              color={focused ? ThemedColors.lightPurple : ThemedColors.text}
+            />
           )}
+          activeBackgroundColor={ThemedColors.darkPurple}
           onPress={() =>
             props.navigation.navigate('AppTabsContainer', {
               screen: 'MeasurementsTab',
-              params: {
-                screen: 'MeasurementsList',
-              },
+              params: { screen: 'MeasurementsList' },
             })
           }
         />
         <DrawerItem
           label="Cadastrar Equipamento"
-          labelStyle={{ color: ThemedColors.text }}
-          icon={({ size, color }) => (
-            <FontAwesomeIcon name="cogs" size={size} color={color} />
+          labelStyle={{
+            color:
+              activeTabName === 'HomeTab' &&
+              activeScreenInTab === 'EquipmentRegisterScreen'
+                ? 'white'
+                : ThemedColors.text,
+          }}
+          focused={
+            activeTabName === 'HomeTab' &&
+            activeScreenInTab === 'EquipmentRegisterScreen'
+          }
+          icon={({ focused, size }) => (
+            <FontAwesomeIcon
+              name="cogs"
+              size={size}
+              color={focused ? ThemedColors.lightPurple : ThemedColors.text}
+            />
           )}
+          activeBackgroundColor={ThemedColors.darkPurple}
           onPress={() =>
             props.navigation.navigate('AppTabsContainer', {
               screen: 'HomeTab',
-              params: {
-                screen: 'EquipmentRegisterScreen',
-              },
+              params: { screen: 'EquipmentRegisterScreen' },
             })
           }
         />
         <DrawerItem
           label="Cadastrar Medidor"
-          labelStyle={{ color: ThemedColors.text }}
-          icon={({ size, color }) => (
-            <FontAwesomeIcon name="tachometer" size={size} color={color} />
+          labelStyle={{
+            color:
+              activeTabName === 'HomeTab' &&
+              activeScreenInTab === 'MeterRegisterScreen'
+                ? 'white'
+                : ThemedColors.text,
+          }}
+          focused={
+            activeTabName === 'HomeTab' &&
+            activeScreenInTab === 'MeterRegisterScreen'
+          }
+          icon={({ focused, size }) => (
+            <FontAwesomeIcon
+              name="tachometer"
+              size={size}
+              color={focused ? ThemedColors.lightPurple : ThemedColors.text}
+            />
           )}
+          activeBackgroundColor={ThemedColors.darkPurple}
           onPress={() =>
             props.navigation.navigate('AppTabsContainer', {
               screen: 'HomeTab',
-              params: {
-                screen: 'MeterRegisterScreen',
-              },
+              params: { screen: 'MeterRegisterScreen' },
             })
           }
         />
         <DrawerItem
           label="Perfil"
-          labelStyle={{ color: ThemedColors.text }}
-          icon={({ size, color }) => (
-            <FontAwesomeIcon name="user" size={size} color={color} />
+          labelStyle={{
+            color:
+              activeTabName === 'ProfileTab' &&
+              activeScreenInTab === 'UserProfileView'
+                ? 'white'
+                : ThemedColors.text,
+          }}
+          focused={
+            activeTabName === 'ProfileTab' &&
+            activeScreenInTab === 'UserProfileView'
+          }
+          icon={({ focused, size }) => (
+            <FontAwesomeIcon
+              name="user"
+              size={size}
+              color={focused ? ThemedColors.lightPurple : ThemedColors.text}
+            />
           )}
+          activeBackgroundColor={ThemedColors.darkPurple}
           onPress={() =>
             props.navigation.navigate('AppTabsContainer', {
               screen: 'ProfileTab',
-              params: {
-                screen: 'UserProfileView',
-              },
+              params: { screen: 'UserProfileView' },
             })
           }
         />
@@ -121,10 +228,10 @@ function CustomDrawerContent(props: DrawerContentComponentProps) {
 const drawerStyles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: ThemedColors.background,
   },
   footer: {
     paddingBottom: Platform.OS === 'ios' ? 20 : 10,
+    paddingTop: 10,
   },
 });
 
@@ -135,15 +242,22 @@ export function MainAppDrawer() {
       screenOptions={{
         drawerPosition: 'right',
         headerShown: false,
-        drawerActiveTintColor: ThemedColors.lightPurple,
-        drawerInactiveTintColor: ThemedColors.text,
+        drawerStyle: {
+          backgroundColor: ThemedColors.backgroundSubmenu,
+        },
+        drawerItemStyle: {
+          marginVertical: 4,
+          marginHorizontal: 8,
+          borderRadius: 8,
+        },
+        drawerLabelStyle: {
+          marginLeft: -20,
+          fontSize: 15,
+        },
+        drawerActiveBackgroundColor: ThemedColors.purple,
       }}
     >
-      <Drawer.Screen
-        name="AppTabsContainer"
-        component={BottomTabNavigator}
-        options={{}}
-      />
+      <Drawer.Screen name="AppTabsContainer" component={BottomTabNavigator} />
     </Drawer.Navigator>
   );
 }
